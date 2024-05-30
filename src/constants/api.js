debf2edb01a9a4e352172800d30b6d973ee3ccf8
Andrey Morogin <andreandreandre10071998@yandex.ru>
2024-05-30 23:14:00 +0300

    fix: fixed import image

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,56 +1,69 @@
+import dzen from './api/dzen.svg'
+import facebook from './api/facebook.svg'
+import instagram from './api/instagram.svg'
+import linkedIn from './api/linked-in.svg'
+import ok from './api/ok.svg'
+import link from './api/portrait.jpg'
+import tg from './api/tg.svg'
+import tiktok from './api/tiktok.svg'
+import viber from './api/viber.svg'
+import vk from './api/vk.svg'
+import watsapp from './api/watsapp.svg'
+import youtube from './api/youtube.svg'
+
 const networkData = [
   {
-    image: '/hubex/src/assets/tg.svg',
+    image: tg,
     link: 'https://web.telegram.org/',
     alt: 'Telegram'
   },
   {
-    image: '/hubex/src/assets/watsapp.svg',
+    image: watsapp,
     link: 'https://www.whatsapp.com/',
     alt: 'WatsApp'
   },
   {
-    image: '/hubex/src/assets/viber.svg',
+    image: viber,
     link: 'https://www.viber.com/',
     alt: 'Viber'
   },
   {
-    image: '/hubex/src/assets/instagram.svg',
+    image: instagram,
     link: 'https://www.instagram.com/',
     alt: 'Instagram'
   },
   {
-    image: '/hubex/src/assets/vk.svg',
-    link: 'https://vk.com/feed',
+    image: vk,
+    link: 'https://vk.com/',
     alt: 'VK'
   },
   {
-    image: '/hubex/src/assets/facebook.svg',
+    image: facebook,
     link: 'https://www.facebook.com/',
     alt: 'Facebook'
   },
   {
-    image: '/hubex/src/assets/youtube.svg',
+    image: youtube,
     link: 'https://www.youtube.com/',
     alt: 'YouTube'
   },
   {
-    image: '/hubex/src/assets/ok.svg',
+    image: ok,
     link: 'https://ok.ru/',
     alt: 'OK'
   },
   {
-    image: '/hubex/src/assets/dzen.svg',
+    image: dzen,
     link: 'https://dzen.ru/',
     alt: 'Dzen'
   },
   {
-    image: '/hubex/src/assets/tiktok.svg',
+    image: tiktok,
     link: 'https://www.tiktok.com/',
     alt: 'TikTok'
   },
   {
-    image: '/hubex/src/assets/linked-in.svg',
+    image: linkedIn,
     link: 'https://ru.linkedin.com/',
     alt: 'Linked_In'
   }
@@ -66,7 +79,7 @@ const personData = {
   location: 'Адрес',
   date: 'Дата рождения',
   rank: 'Звание, учёная степень',
-  imageLink: '/hubex/src/constants/portrait.jpg'
+  imageLink: link
 }
 
 const companyData = {
